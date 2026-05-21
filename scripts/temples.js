b4b81a9f00e3f857d56fe7845643b681080683d0
Danielmatos29 <danielmatosrom2912@gmail.com>
2026-05-20 21:34:32 -0400

    Add current year display to temples.js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,8 +1,11 @@
 const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
 const last_modified = document.querySelector("#lastModified")
+const yearSpan = document.querySelector("#currentyear");
 
 const today = new Date()
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
 
 last_modified.innerHTML = `Last modified: <span class="highlight">${new Intl.DateTimeFormat(
 	"en-US",
